5c2dee70ff0d901c6c698d89e8e360349513ea27
Ezcurra <matialeezcurra@gmail.com>
2018-04-15 21:18:18 -0300

    pods install

---
+++ Example/build/Pods.build/Debug-iphonesimulator/BarcodeScanner.build/DerivedSources/BarcodeScanner_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char BarcodeScannerVersionString[];
+ extern const double BarcodeScannerVersionNumber;
+
+ const unsigned char BarcodeScannerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:BarcodeScanner  PROJECT:Pods-1" "\n";
+ const double BarcodeScannerVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Example/build/Pods.build/Debug-iphonesimulator/Pods-SirioLibanesApp_Tests.build/DerivedSources/Pods_SirioLibanesApp_Tests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_SirioLibanesApp_TestsVersionString[];
+ extern const double Pods_SirioLibanesApp_TestsVersionNumber;
+
+ const unsigned char Pods_SirioLibanesApp_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_SirioLibanesApp_Tests  PROJECT:Pods-1" "\n";
+ const double Pods_SirioLibanesApp_TestsVersionNumber __attribute__ ((used)) = (double)1.;
